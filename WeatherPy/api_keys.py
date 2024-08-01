0e7fd156069ce8ca4bbf270b076f99b9d6478da2
cchea09-YtskQe <cchea09@gmail.com>
2024-07-31 22:53:25 -0500

    WeatherPy started, issue with Data Retrieval

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "cce9d21cb2b39b34feae56e0aabe77ae"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "4767cb8553dc4f85a90e778a53898448"
